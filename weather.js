0b7cd387794e99491d83b72b6410e598775b835e
Nate_River <crimson747@Nate-Riverui-MacBookPro.local>
2020-02-10 02:09:25 +0900

    Vanilla JS

---
+++ weather.js
@@ -0,0 +1,64 @@
+const weather = document.querySelector(".js-weather");
+const API_KEY = "378145080c72ae41a99a38eced2cd8f9";
+const COORDS = "coords";
+
+function getWeather(lat, lng) {
+  fetch(
+    `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`
+  )
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(json) {
+      const temperature = json.main.temp;
+
+      console.log(temperature);
+
+      temperature < 0
+        ? weather.classList.add("cold")
+        : weather.classList.add("hot");
+
+      const place = json.name;
+      weather.innerText = `${temperature} in ${place}`;
+    });
+}
+
+function saveCoords(coordsObj) {
+  localStorage.setItem(COORDS, JSON.stringify(coordsObj));
+}
+
+function handleGeoError() {
+  console.log("Can't access geo location");
+}
+
+function handleGeoSucess(position) {
+  const latitude = position.coords.latitude;
+  const longitude = position.coords.longitude;
+  const coordsObj = {
+    latitude,
+    longitude
+  };
+  saveCoords(coordsObj);
+  getWeather(latitude, longitude);
+}
+
+function askForCoords() {
+  navigator.geolocation.getCurrentPosition(handleGeoSucess, handleGeoError);
+}
+
+function loadCoords() {
+  const loadedCoords = localStorage.getItem(COORDS);
+
+  if (loadedCoords === null) {
+    askForCoords();
+  } else {
+    const parseCoords = JSON.parse(loadedCoords);
+    getWeather(parseCoords.latitude, parseCoords.longitude);
+  }
+}
+
+function init() {
+  loadCoords();
+}
+
+init();
